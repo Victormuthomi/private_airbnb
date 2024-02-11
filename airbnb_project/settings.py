c91f5b19f83892519dc65c5285da04b898e365dd
victormuthomi <victormuthomi100@gmail.com>
2024-02-11 21:59:39 +0300

    Ready for deployement in platform.sh

--- airbnb_project/settings.py
+++ airbnb_project/settings.py
@@ -144,3 +144,29 @@ MEDIA_URL = '/media/'
 
 #static files
 STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
+
+# Platform.sh settings.
+from platformshconfig import Config
+
+config = Config()
+if config.is_valid_platform():
+    ALLOWED_HOSTS.append('.platformsh.site')
+    DEBUG = False
+
+    if config.appDir:
+        STATIC_ROOT = Path(config.appDir) / 'static'
+    if config.projectEntropy:
+        SECRET_KEY = config.projectEntropy
+
+    if not config.in_build():
+        db_settings = config.credentials('database')
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': db_settings['path'],
+                'USER': db_settings['username'],
+                'PASSWORD': db_settings['password'],
+                'HOST': db_settings['host'],
+                'PORT': db_settings['port'],
+    },
+}
