58564748c24802fc42abb59d679141e0f28bbd05
victormuthomi <victormuthomi100@gmail.com>
2024-02-13 00:58:21 +0300

    final render commit.

--- airbnb_project/settings.py
+++ airbnb_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-0^naqut&)b+b-e45yx+=*nwf66ft7mee-_()5kfabkur06mpds
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
